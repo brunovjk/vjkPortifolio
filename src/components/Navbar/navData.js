d15dcb081ebd3aa28e44f83df2ab2dcf5db4ab4e
brunovjk <brunovjk@gmail.com>
2022-10-21 15:18:30 -0300

    client: brunovjkPortifolio-create-10/21/22

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,32 @@
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/hamishMW',
+    icon: 'twitter',
+  },
+  {
+    label: 'Figma',
+    url: 'https://www.figma.com/@Hamish',
+    icon: 'figma',
+  },
+  {
+    label: 'Github',
+    url: 'https://github.com/HamishMW',
+    icon: 'github',
+  },
+];
---
+++ src/layouts/Home/Home.js
@@ -0,0 +1,157 @@
+import gamestackTexture2Large from 'assets/gamestack-list-large.jpg';
+import gamestackTexture2Placeholder from 'assets/gamestack-list-placeholder.jpg';
+import gamestackTexture2 from 'assets/gamestack-list.jpg';
+import gamestackTextureLarge from 'assets/gamestack-login-large.jpg';
+import gamestackTexturePlaceholder from 'assets/gamestack-login-placeholder.jpg';
+import gamestackTexture from 'assets/gamestack-login.jpg';
+import sliceTextureLarge from 'assets/slice-app-large.jpg';
+import sliceTexturePlaceholder from 'assets/slice-app-placeholder.jpg';
+import sliceTexture from 'assets/slice-app.jpg';
+import sprTextureLarge from 'assets/spr-lesson-builder-dark-large.jpg';
+import sprTexturePlaceholder from 'assets/spr-lesson-builder-dark-placeholder.jpg';
+import sprTexture from 'assets/spr-lesson-builder-dark.jpg';
+import { Footer } from 'components/Footer';
+import { Meta } from 'components/Meta';
+import { Intro } from 'layouts/Home/Intro';
+import { Profile } from 'layouts/Home/Profile';
+import { ProjectSummary } from 'layouts/Home/ProjectSummary';
+import { useEffect, useRef, useState } from 'react';
+import styles from './Home.module.css';
+
+const disciplines = ['Developer', 'Fanatic', 'Engineer', 'Dreamer', 'Designer'];
+
+export const Home = () => {
+  const [visibleSections, setVisibleSections] = useState([]);
+  const [scrollIndicatorHidden, setScrollIndicatorHidden] = useState(false);
+  const intro = useRef();
+  const projectOne = useRef();
+  const projectTwo = useRef();
+  const projectThree = useRef();
+  const details = useRef();
+
+  useEffect(() => {
+    const sections = [intro, projectOne, projectTwo, projectThree, details];
+
+    const sectionObserver = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const section = entry.target;
+            observer.unobserve(section);
+            if (visibleSections.includes(section)) return;
+            setVisibleSections(prevSections => [...prevSections, section]);
+          }
+        });
+      },
+      { rootMargin: '0px 0px -10% 0px', threshold: 0.1 }
+    );
+
+    const indicatorObserver = new IntersectionObserver(
+      ([entry]) => {
+        setScrollIndicatorHidden(!entry.isIntersecting);
+      },
+      { rootMargin: '-100% 0px 0px 0px' }
+    );
+
+    sections.forEach(section => {
+      sectionObserver.observe(section.current);
+    });
+
+    indicatorObserver.observe(intro.current);
+
+    return () => {
+      sectionObserver.disconnect();
+      indicatorObserver.disconnect();
+    };
+  }, [visibleSections]);
+
+  return (
+    <div className={styles.home}>
+      <Meta
+        title="Blockchain Developer"
+        description="Blockchain portfolio of Bruno Rocha — Excited about the new world!"
+      />
+      <Intro
+        id="intro"
+        sectionRef={intro}
+        disciplines={disciplines}
+        scrollIndicatorHidden={scrollIndicatorHidden}
+      />
+      <ProjectSummary
+        id="project-1"
+        sectionRef={projectOne}
+        visible={visibleSections.includes(projectOne.current)}
+        index={1}
+        title="vjkNFT is a on-chain NFT͟ Collection"
+        description="vjkNFT is a dApp developed by Bruno Rocha. Our application consists of a Smart Contract and a React App to interact with the blockchain, where this contract is deployed."
+        buttonText1="Live"
+        buttonLink1="https://vjknft.brunovjk.com/"
+        buttonText2="Github"
+        buttonLink2="https://github.com/brunovjk/vjkNFT"
+        model={{
+          type: 'laptop',
+          alt: 'vjkNFT NFT͟ Collection',
+          textures: [
+            {
+              srcSet: [sprTexture, sprTextureLarge],
+              placeholder: sprTexturePlaceholder,
+            },
+          ],
+        }}
+      />
+      <ProjectSummary
+        id="project-2"
+        alternate
+        sectionRef={projectTwo}
+        visible={visibleSections.includes(projectTwo.current)}
+        index={2}
+        title="SaudeVapor"
+        description="Cannabis contains at least 49 active ingredients/cannabinoids, which provide diverse effects on our body. I created a Unique Collection, using these 49 components and their Main Characteristics and Possible Therapeutic Benefits."
+        buttonText1="Live"
+        buttonLink1="https://saudevapor.com/"
+        buttonText2="Github"
+        buttonLink2="https://github.com/brunovjk/SaudeVapor"
+        model={{
+          type: 'phone',
+          alt: 'SaudeVapor',
+          textures: [
+            {
+              srcSet: [gamestackTexture, gamestackTextureLarge],
+              placeholder: gamestackTexturePlaceholder,
+            },
+            {
+              srcSet: [gamestackTexture2, gamestackTexture2Large],
+              placeholder: gamestackTexture2Placeholder,
+            },
+          ],
+        }}
+      />
+      <ProjectSummary
+        id="project-3"
+        sectionRef={projectThree}
+        visible={visibleSections.includes(projectThree.current)}
+        index={3}
+        title="Chainlink Fall 2022 Hackathon"
+        description="Working in a awesome project. News soon."
+        buttonText1="Join the Hackathon"
+        buttonLink1="https://chain.link/hackathon"
+        model={{
+          type: 'laptop',
+          alt: 'Chainlink Fall 2022 Hackathon',
+          textures: [
+            {
+              srcSet: [sliceTexture, sliceTextureLarge],
+              placeholder: sliceTexturePlaceholder,
+            },
+          ],
+        }}
+      />
+      <Profile
+        sectionRef={details}
+        visible={visibleSections.includes(details.current)}
+        id="details"
+      />
+      <Footer />
+    </div>
+  );
+};
